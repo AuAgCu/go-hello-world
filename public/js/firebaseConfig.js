fb4f7e13846b8869da8584ed8f4add10c6d59137
MusashiOkuoka <musashi.okuoka@i-standard.biz>
2021-11-02 21:24:56 +0900

    jwt取得

---
+++ public/js/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCFjTtaTVcNEOQVqJt3S9FcBmywd11IORs",
+    authDomain: "go-test-5007f.firebaseapp.com",
+    projectId: "go-test-5007f",
+    storageBucket: "go-test-5007f.appspot.com",
+    messagingSenderId: "675509811756",
+    appId: "1:675509811756:web:e9658e52743d9a6680af89"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
