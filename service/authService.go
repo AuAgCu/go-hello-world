4f4e511991d4966ac4a7a2cf28f64bdc47052359
MusashiOkuoka <musashi.okuoka@i-standard.biz>
2021-11-04 20:17:27 +0900

    userId取得

--- service/authService.go
+++ service/authService.go
@@ -2,11 +2,16 @@ package service
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	firebase "firebase.google.com/go"
+	"firebase.google.com/go/auth"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
 )
@@ -15,6 +20,7 @@ var AuthServiceSingleton = &authServiceImpl{}
 
 type AuthService interface {
 	Verify(jwtToken string) bool
+	GetUserId(jwtToken string) string
 }
 
 type authServiceImpl struct {
@@ -37,13 +43,61 @@ func (authservice authServiceImpl) Verify(jwtToken string) bool {
 		return false
 	}
 
-	println("huga")
 	token, err := auth.VerifyIDToken(context.Background(), jwtToken)
 	if err != nil {
 		fmt.Printf("error verifying ID token: %v\n", err)
 		return false
 	}
 
+	userId := authservice.GetUserId(jwtToken)
+
 	log.Printf("Verified ID token: %v\n", token)
+	log.Printf("Verified ID token: %s\n", userId)
 	return true
+}
+
+func (authService authServiceImpl) GetUserId(jwtToken string) string {
+	hcs, err := authService.decomposeFB(jwtToken)
+	if err != nil {
+		log.Fatalln("Error : ", err)
+	}
+
+	payload, err := authService.decodeClaimFB(hcs[1])
+	if err != nil {
+		log.Fatalln("Error : ", err)
+	}
+
+	return payload.Subject
+}
+
+//DecomposeFB  JWTをHeader, claims, 署名に分解
+func (_ authServiceImpl) decomposeFB(jwt string) ([]string, error) {
+	hCS := strings.Split(jwt, ".")
+	if len(hCS) == 3 {
+		return hCS, nil
+	}
+	return nil, errors.New("Error jwt str decompose: inccorrect number of segments")
+
+}
+
+type FireBaseCustomToken struct {
+	auth.Token
+	Email string `json:"email"`
+}
+
+//DecodeClaimFB JWTのclaims部分をFireBaseCustomTokenの構造体にデコード
+func (_ authServiceImpl) decodeClaimFB(payload string) (*FireBaseCustomToken, error) {
+	payloadByte, err := base64.RawURLEncoding.DecodeString(payload)
+	if err != nil {
+		return nil, errors.New("Error jwt token decode: " + err.Error())
+	}
+
+	var tokenJSON FireBaseCustomToken
+	err = json.Unmarshal(payloadByte, &tokenJSON)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return nil, errors.New("Error jwt token unmarshal: " + err.Error())
+	}
+
+	return &tokenJSON, nil
 }
